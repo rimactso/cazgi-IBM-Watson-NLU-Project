d1cf4f9e4350a3a4e4fd41d798a22b18d4a62ebe
rimactso <rimact@mail.com>
2021-02-27 12:05:33 +0000

    added from lab

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = new express();
 
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+function getNLUInstance(){
+    let api_key=process.env.API_KEY;
+    let api_url=process.env.API_URL;
+    const nluv = require('ibm-watson/natural-language-understanding/v1');
+    const {IamAuthenticator}= require('ibm-watson/auth');
+    const nlu = nluv({
+        version:'2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey:api_key,
+        }),
+        serviceURL: api_url,
+    });
+    return nlu;
+}
+
+
+
 app.use(express.static('client'))
 
 const cors_app = require('cors');
